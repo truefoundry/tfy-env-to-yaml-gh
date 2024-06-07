02656ef18ae2a2884ff0ee7576870f2c31a8d513
Shubham Rai <shubham.rai.iitg@gmail.com>
2024-06-07 13:04:34 +0530

    using yaml library to preserve comments

--- index.mjs
+++ index.mjs
@@ -1,33 +1,42 @@
-import * as fs from 'fs'
-import dotenv from 'dotenv'
-import yaml from 'js-yaml'
-import core from '@actions/core'
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+import yaml from 'yaml';
+import core from '@actions/core';
 
 function main() {
-  const envPath = core.getInput('envPath');
-  const outputFile = core.getInput('outputFile');
-  const imageTag = core.getInput('imageTag');
-  const subPath = core.getInput('subPath', { required: false });
-  const { parsed } = dotenv.config({ path: envPath })
-  const data = fs.readFileSync(outputFile)
-  let yamlIn = yaml.load(data, 'utf8')
+  // const envPath = core.getInput('envPath');
+  // const outputFile = core.getInput('outputFile');
+  // const imageTag = core.getInput('imageTag');
+  // const subPath = core.getInput('subPath', { required: false });
+
+  const envPath = '.env.default';
+  const outputFile = 'values.yaml';
+  const imageTag = '<test-tag>';
+  const subPath = 'sfyManifestService';
+
+  const { parsed } = dotenv.config({ path: envPath });
+  const data = fs.readFileSync(outputFile, 'utf8');
+  const doc = yaml.parseDocument(data);
+  
   if (subPath) {
-    let currentObj = yamlIn;
+    let currentObj = doc;
     const subPathKeys = subPath.split('.');
     for (let i = 0; i < subPathKeys.length - 1; i++) {
       const key = subPathKeys[i];
-      if (!currentObj[key]) {
-        currentObj[key] = {};
+      if (!currentObj.get(key)) {
+        currentObj.set(key, {});
       }
-      currentObj = currentObj[key];
+      currentObj = currentObj.get(key);
     }
-    currentObj[subPathKeys[subPathKeys.length - 1]]['env'] = parsed;
-    currentObj[subPathKeys[subPathKeys.length - 1]]['imageTag'] = imageTag;
+    currentObj.get(subPathKeys[subPathKeys.length - 1]).set('env', parsed);
+    currentObj.get(subPathKeys[subPathKeys.length - 1]).set('imageTag', imageTag);
   } else {
-    yamlIn['env'] = parsed;
-    yamlIn['imageTag'] = imageTag;
+    doc.set('env', parsed);
+    doc.set('imageTag', imageTag);
   }
-  const yamlOut = yaml.dump(yamlIn);
+  
+  const yamlOut = doc.toString();
   fs.writeFileSync(outputFile, yamlOut);
 }
-main()
+
+main();
